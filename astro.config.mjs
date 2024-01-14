586751196428d9144a6934c17b27f25871c84c68
mcb10 <38315618+mcb10@users.noreply.github.com>
2024-01-14 22:13:05 +0000

    Added Google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
+import partytown from '@astrojs/partytown';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mcb10.github.io'
-  // base: '/my-repo',
-  ,
-
-  integrations: [preact(), tailwind()]
+  site: 'https://mcb10.github.io',
+  integrations: [
+    preact(), 
+    tailwind(),
+    partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),
+  ]
 });
