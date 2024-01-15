6f772e9d1389f90592fdbd58c4b52da404b2fbf7
mcb10 <38315618+mcb10@users.noreply.github.com>
2024-01-15 17:38:47 +0000

    Improved ProductCard and removed 'tailwind' lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import partytown from '@astrojs/partytown';
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mcb10.github.io',
   integrations: [
-    preact(), 
-    tailwind(),
+    preact(),
     partytown({
 			config: {
 			  forward: ["dataLayer.push"],
