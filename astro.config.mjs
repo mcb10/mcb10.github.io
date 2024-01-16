bc755ba271ae88fbc616d7b6326369feb9896c6a
mcb10 <38315618+mcb10@users.noreply.github.com>
2024-01-16 22:01:09 +0000

    Integrated icons and defined scope for 'vads.astro'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import partytown from '@astrojs/partytown';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mcb10.github.io',
-  integrations: [
-    preact(),
-    partytown({
-			config: {
-			  forward: ["dataLayer.push"],
-			},
-		}),
-  ]
+  integrations: [preact(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), icon()]
 });
